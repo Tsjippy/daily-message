e9103144492dfb2b46b2a8e569326d29d84ebe96
Tsjippy <ewaldha@hotmail.com>
2026-09-12 07:26:01 +0200

    changed: removed 'tested up to'

--- tsjippy-daily-message.php
+++ tsjippy-daily-message.php
@@ -10,7 +10,6 @@ namespace TSJIPPY\DAILYMESSAGE;
  * AuthorURI:            harmseninnigeria.nl
  * Requires at least:    6.3
  * Requires PHP:         8.3
- * Tested up to:         7.1
  * Plugin URI:           https://github.com/Tsjippy/daily-message
  * Tested:               7.1
  * TextDomain:           tsjippy
