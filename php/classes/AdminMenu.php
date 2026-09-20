cbff738b74a4384fafc1d2ba3e493129a484e0fa
Tsjippy <ewaldha@hotmail.com>
2026-09-20 16:48:45 +0200

    changed: load minification js depending on SCRIPT_DEBUG constant

--- php/classes/AdminMenu.php
+++ php/classes/AdminMenu.php
@@ -33,7 +33,7 @@ class AdminMenu extends \TSJIPPY\ADMIN\SubAdminMenu
      */
     public function settings($parent)
     {
-        wp_enqueue_script_module('@tsjippy/message_admin', TSJIPPY\pathToUrl(PLUGINPATH . 'js/admin.min.js'), array('@tsjippy/main'), PLUGINVERSION);
+        wp_enqueue_script_module('@tsjippy/message_admin', TSJIPPY\pathToUrl(PLUGINPATH . 'js/admin' . TSJIPPY\JSEXTENSION), array('@tsjippy/main'), PLUGINVERSION);
 
         ob_start();
 
